c531653069d7791c43fee5f0b30eaaffad2fa534
mieszko4 <mieszko4@gmail.com>
2021-09-28 12:42:53 +0200

    Prepare for 5.7.1.645

--- README.md
+++ README.md
@@ -14,12 +14,13 @@ Pod includes sdk list:
 ### Usage
 Update your Podfile:
 ```
-pod 'ZoomSDK', '5.7.1.644'
+pod 'ZoomSDK', '5.7.1.645'
 ```
 
 
 ### Versions
 
+- 5.7.1.645
 - 5.7.1.644
 - 5.5.12511.0421
 - 5.4.54802.0124.1
--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 
-REPO=https://github.com/zvs001/zoom-sdk-pods/releases/download/zoom-releases
-FILE=zoom-sdk-ios-5.7.1.644.zip
+REPO=https://github.com/zoom-us-community/zoom-sdk-pods/releases/download/zoom-releases
+FILE=zoom-sdk-ios-5.7.1.645.zip
 
 wget "$REPO/$FILE"
 tar -xf "$FILE"

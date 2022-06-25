0aa6a5ed966d9122ddb7d815f35c910011df1da7
mieszko4 <mieszko4@gmail.com>
2022-06-25 15:43:33 +0200

    Remove dependency - curl is pre-installed

--- make.sh
+++ make.sh
@@ -2,6 +2,6 @@
 REPO=https://github.com/zoom-us-community/zoom-sdk-pods/releases/download/zoom-releases
 FILE=zoom-sdk-ios-5.11.0.3907.zip
 
-wget "$REPO/$FILE"
+curl "$REPO/$FILE" -O -J -L
 tar -xf "$FILE"
 rm "$FILE"

cfa0cd93882ab038259fe5f82f3d2442a586f5a1
mieszko4 <mieszko4@gmail.com>
2024-03-18 08:31:38 +0100

    feat: 5.17.11.14222

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 
 REPO=https://github.com/zoom-us-community/zoom-sdk-pods/releases/download/zoom-releases
-FILE=zoom-sdk-ios-5.14.11.8690.zip
+FILE=zoom-sdk-ios-5.17.11.14222.zip
 
 curl "$REPO/$FILE" -O -J -L
 tar -xf "$FILE"
